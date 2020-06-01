bd7c5f39bd83f7578d51bdb23639f247b6d90166
CharanRoot <charan.sjd@gmail.com>
2020-06-01 11:30:22 -0500

    first commit

---
+++ precache-manifest.12fd71575fd0bd566c0e01eb0ff251df.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "372e5dac17f9eb8b2a8a60b54f8084ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "735a4ba606f9f78e508e",
+    "url": "/static/css/2.a538dfa1.chunk.css"
+  },
+  {
+    "revision": "21b7f81ece89263d333d",
+    "url": "/static/css/main.c5f93cc7.chunk.css"
+  },
+  {
+    "revision": "735a4ba606f9f78e508e",
+    "url": "/static/js/2.2d474db4.chunk.js"
+  },
+  {
+    "revision": "0ffeeb0f2c5a33fafedbe5510b721aa1",
+    "url": "/static/js/2.2d474db4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21b7f81ece89263d333d",
+    "url": "/static/js/main.8e132500.chunk.js"
+  },
+  {
+    "revision": "2577406561f70fa36467",
+    "url": "/static/js/runtime-main.3ff8ede7.js"
+  },
+  {
+    "revision": "1cedb6e919bfed6a2c1ec00b5d8ee620",
+    "url": "/static/media/UploadIcon.1cedb6e9.svg"
+  }
+]);
